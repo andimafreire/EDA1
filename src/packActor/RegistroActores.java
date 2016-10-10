998ccfbd4e9572d36fb3a822189ea5d7c6648252
Andima Freire <andima.freire@gmail.com>
2016-10-10 09:59:24 +0200

    hasmap empezado

--- src/packActor/RegistroActores.java
+++ src/packActor/RegistroActores.java
@@ -1,18 +1,23 @@
 package packActor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistroActores {
-	private ListaActores actores;
+	private Map<String,Actor> mapaActores;
 	private RegistroActores elRegistroActores;
 	
-	private RegistroActores() {}
+	private RegistroActores() {
+		mapaActores = new HashMap<>();
+	}
 	
 	public RegistroActores getRegistroActores() {
 		if (elRegistroActores == null) elRegistroActores = new RegistroActores();
 		return elRegistroActores;
 	}
 	
-	public Actor buscarActor(String pApel, String pNom) {
-		return actores.buscarActor(pApel, pNom);		
+	public Actor buscarActor(String pNomApe) {
+		return mapaActores.get(pNomApe);
 	}
 
 }
--- src/packPrincipal/Gestionator.java
+++ src/packPrincipal/Gestionator.java
@@ -9,7 +9,27 @@ public class Gestionator {
 	
 	private Gestionator() {}
 	
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		System.out.println("seleccione operacion:");
+		System.out.println();
+		System.out.println(" 1. cargar fichero ");
+		System.out.println(" 2. buscar un actor ");
+		System.out.println(" 3. insertar un actor ");
+		System.out.println(" 4. mostrar peliculas rodadas por un actor ");
+		System.out.println(" 5. mostrar actores de una pelicula ");
+		System.out.println(" 6. incrementar la recaudacion de una pelicula ");
+		System.out.println(" 7. borrar actor ");
+		System.out.println(" 8. exportar registro de peliculas a fichero ");
+		System.out.println(" 9. obtener lista ordenada de actores ");
+		System.out.println();
+		System.out.println("------------------------------------------------------------");
+		int seleccion;
+		Scanner sc = new Scanner(System.in);
+		seleccion=sc.nextInt();
+		
+		
+		
+	}
 	
 	public Gestionator getGestionator() {
 		if (elGestionator == null) elGestionator = new Gestionator();
@@ -36,7 +56,7 @@ public class Gestionator {
 	private void buscarActor() {
 		
 	}
-	
+
 	private void insertarActor() {
 		
 	}
