578191a1326809f6fae66b0ad6be65d4a1eb431d
Mikel Abad <MikelAbadG@gmail.com>
2016-10-13 14:00:39 +0200

    Clase StopWatch

---
+++ src/packPrincipal/StopWatch.java
@@ -0,0 +1,19 @@
+package packPrincipal;
+
+public class StopWatch { 
+
+    private final long start;
+
+   /** Create a stopwatch object. */
+    public StopWatch() {
+        start = System.currentTimeMillis();
+    } 
+
+   /**
+     * Return elapsed time (in seconds) since this object was created.
+     */
+    public long elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start);
+    }
+} 
