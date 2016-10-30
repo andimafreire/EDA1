5b8553c333b9e67c858f66771b0df369f14ec91e
Andima Freire <andima.freire@gmail.com>
2016-10-30 12:43:26 +0100

    falta iterator y la lista ordenda de la parte listas enlazadas

--- practica2/packListasEnlazadas/CircularLinkedList.java
+++ practica2/packListasEnlazadas/CircularLinkedList.java
@@ -6,125 +6,173 @@ import java.util.NoSuchElementException;
 public class CircularLinkedList<T> implements ListADT<T> {
 
 	// Atributos
-	protected Node<T> last;  //Apuntador al �ltimo
-	protected String descr;  //Descripci�n
+	protected Node<T> last; // Apuntador al �ltimo
+	protected String descr; // Descripci�n
 	protected int count;
 
 	// Constructor
 	public CircularLinkedList() {
-	    last = null;
+		last = null;
 		descr = "";
 		count = 0;
 	}
-	
+
 	public void setDescr(String nom) {
-	  descr = nom;
+		descr = nom;
 	}
 
 	public String getDescr() {
-	  return descr;
+		return descr;
 	}
 
 	public T removeFirst() {
-	    // Elimina el primer elemento de la lista
-	    	// Precondición: la lista tiene al menos un elemento
-	   	 // COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		 Node<T> aux=last.next;
-	   	 if(last!=last.next){   	 
-	   	 last.next=aux.next;
-	   	}   	 
-	   	 else{
-	   		 last=null;   		
-	   	 }
-	   	 return (aux.data);
-	   	 
-	    }
+		// Elimina el primer elemento de la lista
+		// Precondición: la lista tiene al menos un elemento
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		Node<T> aux = last.next;
+		if (last != last.next) {
+			last.next = aux.next;
+		} else {
+			last = null;
+		}
+		return (aux.data);
 
-
-	    public T removeLast() {
-	    // Elimina el último elemento de la lista
-	    	// Precondición: la lista tiene al menos un elemento   	
-		 // COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		Node<T> aux=last;
-		if (last.next!=last){
-	   	 Node<T> act=last.next;
-		
-		
-	   		 while(last!=act.next){
-	   		 act=act.next;   
-			 
-	   		 }
-	   	 act.next=last.next;
-	   	 this.last=act;   	
-	}
-	else{
-	this.last=null;
-	}
-	return (aux.data);
-	   		
 	}
 
+	public T removeLast() {
+		// Elimina el último elemento de la lista
+		// Precondición: la lista tiene al menos un elemento
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		Node<T> aux = last;
+		if (last.next != last) {
+			Node<T> act = last.next;
 
+			while (last != act.next) {
+				act = act.next;
 
+			}
+			act.next = last.next;
+			this.last = act;
+		} else {
+			this.last = null;
+		}
+		return (aux.data);
+
+	}
 
 	public T remove(T elem) {
-	// Elimina un elemento concreto de la lista
-		// COMPLETAR EL C�DIGO Y CALCULAR EL COSTE
+		// Elimina un elemento concreto de la lista
+		// coste: O(n)
+		Node<T> act = this.last;
+		while (act.next!=this.last){
+			if (act.next.data.equals(elem)){
+				act.next = act.next.next;
+				return elem;
+			}
+		}
+		if (act.next.data.equals(elem)){
+			act.next = act.next.next;
+			return elem;
+		}
+		return null;
+		
 	}
 
 	public T first() {
-	//Da acceso al primer elemento de la lista
-	      if (isEmpty())
-	          return null;
-	      else return last.next.data;
+		// Da acceso al primer elemento de la lista
+		if (isEmpty())
+			return null;
+		else
+			return last.next.data;
 	}
 
 	public T last() {
-	//Da acceso al �ltimo elemento de la lista
-	      if (isEmpty())
-	          return null;
-	      else return last.data;
+		// Da acceso al �ltimo elemento de la lista
+		if (isEmpty())
+			return null;
+		else
+			return last.data;
 	}
 
 	public boolean contains(T elem) {
-
-		   
+		Node<T> act = this.last;
+		while (act.next!=this.last){
+			if (act.next.data.equals(elem)){
+				return true;
+			}
+		}
+		if (act.next.data.equals(elem)){
+			return true;
+		}
+		return false;
 	}
 
 	public T find(T elem) {
-	// Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no est�
+		// Determina si la lista contiene un elemento concreto, y develve su
+		// referencia, null en caso de que no est�
 		// COMPLETAR EL C�DIGO Y CALCULAR EL COSTE
-
+		Node<T> act = this.last;
+		while (act.next!=this.last){
+			if (act.next.data.equals(elem)){
+				return act.next.data;
+			}
+		}
+		if (act.next.data.equals(elem)){
+			return act.next.data;
+		}
+		return null;
 	}
 
-	public boolean isEmpty() {	//Determina si la lista est� vac�a
-		return last == null;}
-	
-	public int size() {			//Determina el n�mero de elementos de la lista
-		return count;}
-	
-	   public Iterator<T> iterator() {return new ListIterator();} 
+	public boolean isEmpty() { // Determina si la lista est� vac�a
+		return last == null;
+	}
 
-	   // An iterator doesn't implement remove() since it's optional 
-	   private class ListIterator implements Iterator<T> { 
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+	public int size() { // Determina el n�mero de elementos de la lista
+		return count;
+	}
 
-	   } // private class
-		
-		
-         public void visualizarNodos() {
-			System.out.println(this.toString());
+	public Iterator<T> iterator() {
+		return new ListIterator();
+	}
+
+	// An iterator doesn't implement remove() since it's optional
+	private class ListIterator implements Iterator<T> {
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
 		}
 
 		@Override
-		public String toString() {
-			String result = new String();
-			Iterator<T> it = iterator();
-			while (it.hasNext()) {
-				T elem = it.next();
-				result = result + "[" + elem.toString() + "] \n";
-			}	
-			return "SimpleLinkedList " + result + "]";
+		public T next() {
+			// TODO Auto-generated method stub
+			return null;
 		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		
+
+	} // private class
+
+	public void visualizarNodos() {
+		System.out.println(this.toString());
+	}
+
+	@Override
+	public String toString() {
+		String result = new String();
+		Iterator<T> it = iterator();
+		while (it.hasNext()) {
+			T elem = it.next();
+			result = result + "[" + elem.toString() + "] \n";
+		}
+		return "SimpleLinkedList " + result + "]";
+	}
 
 }
--- practica2/packListasEnlazadas/UnorderedCircularLinkedList.java
+++ practica2/packListasEnlazadas/UnorderedCircularLinkedList.java
@@ -5,18 +5,38 @@ public class UnorderedCircularLinkedList<T> extends CircularLinkedList<T> implem
 	public void addToFront(T elem) {
 	// Añade un elemento al comienzo
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		Node<T> nuevo = new Node<T>(elem);
+		nuevo.next = this.last.next;
+		this.last.next = nuevo;
 	}
 
 	public void addToRear(T elem) {
 	// Añade un elemento al final 
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		Node<T> nuevo = new Node<T>(elem);
+		if (this.last == null) {
+			this.last = nuevo;
+			nuevo.next = nuevo;
+		}
+		else {
+			nuevo.next = this.last.next;
+			this.last.next = nuevo;
+			this.last=nuevo;	
+		}
 	}
 	
 	public void addAfter(T elem, T target) {
 	// Añade elem detrás de otro elemento concreto, target, que ya se encuentra en la lista
 		// ¡COMPLETAR OPCIONAL!
+		Node<T> nuevo = new Node<T>(elem);
+		Node<T> act = this.last;
+		boolean insertado = false;
+		while (!insertado){
+			if (act.data.equals(target)){
+				nuevo.next = act.next;
+				act.next = nuevo;
+				insertado = true;
+			}
+		}
 	}
-
 }
