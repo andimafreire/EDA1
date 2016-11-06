d482cce5042c29d0e11b14b7796495dc524bff0e
Andima Freire <andima.freire@gmail.com>
2016-11-06 12:53:59 +0100

    clase lista enlazada ordenada implementada.

--- practica2/packListasEnlazadas/OrderedCircularLinkedList.java
+++ practica2/packListasEnlazadas/OrderedCircularLinkedList.java
@@ -3,6 +3,38 @@ package packListasEnlazadas;
 public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
 	
 	public void add(T elem){
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		// lista ordenada de menor a mayor. 
+		// coste: debido a que se recorre la lista hasta una media de n/2 podemos decir que tiene coste lineal O(n).
+		Node<T> nuevo = new Node<T>(elem);
+		if(last==null){
+			last = nuevo;
+		}
+		else{
+			boolean anadido = false;
+			Node<T> act = this.last.next;
+			Node<T> ant = this.last;
+			while (act!=this.last && !anadido){
+				if (act.data.toString().compareTo(elem.toString())>0){ //si act es mayor que elem introduce elem delante.
+					ant.next = nuevo;
+					nuevo.next = act;
+					anadido = true;
+				}
+				else {
+					ant = act;
+					act = act.next;
+				}
+			}
+			if (!anadido){
+				if(act.next.data.toString().compareTo(elem.toString())>0){ //si act es mayor que elem introduce elem delante.
+					ant.next = nuevo;
+					nuevo.next = act;
+				}
+				else{
+					nuevo.next = last.next;
+					last.next = nuevo;
+					last = nuevo;
+				}
+			}		
+		}	
 	}
 }
